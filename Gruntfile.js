5ed527f15fc06388f05ef626291f9ba19698283e
Ralf S. Engelschall <rse@engelschall.com>
2014-03-07 20:39:29 +0100

    add ESLint support to build environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,12 @@ module.exports = function (grunt) {
             gruntfile:   [ "Gruntfile.js" ],
             sourcefiles: [ "jquery.stage.js" ]
         },
+        eslint: {
+            options: {
+                config: "eslint.json"
+            },
+            target: [ "jquery.stage.js" ],
+        },
         uglify: {
             options: {
                 preserveComments: "some",
@@ -52,7 +58,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-eslint");
 
-    grunt.registerTask("default", [ "jshint", "uglify" ]);
+    grunt.registerTask("default", [ "jshint", "eslint", "uglify" ]);
 };
 
